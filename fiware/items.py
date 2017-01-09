f1f6911da8247864b065174f893f86e92f60ed41
Peter <peter.muryshkih@iais.fraunhofer.de>
2017-01-09 16:29:44 +0100

    added new field 'meta' to scrape the Catalogue

--- fiware/items.py
+++ fiware/items.py
@@ -8,12 +8,13 @@
 import scrapy
 
 
-# each property reflects a tab in the FIWARE Catalogue UI, see e.g. 
+# each property reflects a tab in the FIWARE Catalogue UI, see e.g.
 # http://catalogue.fiware.org/enablers/complex-event-processing-cep-proactive-technology-online
 # Guide: https://forge.fiware.org/plugins/mediawiki/wiki/fiware/index.php/Working_with_the_FIWARE_catalogue#Guidelines_on_what_to_write
 class FiwareEnablerItem(scrapy.Item):
-    
+
     url = scrapy.Field()
     enabler = scrapy.Field()
     text = scrapy.Field()
+    meta = scrapy.Field()
 
--- fiware/spiders/fige_spider.py
+++ fiware/spiders/fige_spider.py
@@ -8,7 +8,7 @@ class FigeSpider(CrawlSpider):
     allowed_domains = ["catalogue.fiware.org"]
     start_urls = [
         "http://catalogue.fiware.org/enablers/"
-	
+
     ]
     rules = (
         Rule(LinkExtractor(allow=(),deny=('^http://catalogue.fiware.org/enablers/%20http:')), callback="parse_items", follow=True),)
@@ -16,14 +16,18 @@ class FigeSpider(CrawlSpider):
     def parse_items(self, response):
             item = FiwareEnablerItem()
             title = ' '.join( response.xpath("//*[@class='title']//text()").extract());
-            if title=='Generic Enablers' or "/enablers/%20" in response.url or "/email/node/" in response.url: 
+            if title=='Generic Enablers' or "/enablers/%20" in response.url or "/email/node/" in response.url:
 		print "skip: " + title + " / " + response.url
                 return
 	    print "processing title: " + title +   " from url: " + response.url
 
-            item['enabler'] = title;
+            item['enabler'] = title # will be used to identify the Enabler
             item['url'] = response.url
-            item['text'] = ' ' .join( response.xpath("//*[contains(concat(' ', @class, ' '), ' field-type-text-with-summary ')]//text()").extract())
+            # main text block in his layout
+	    item['text'] = ' ' .join( response.xpath("//*[contains(concat(' ', @class, ' '), ' field-type-text-with-summary ')]//text()").extract())
+	    # information with contact person etc.
+	    item['meta'] = ' '.join ( response.xpath("//*[@id=\"block-system-main\"]/div/div/div[2]/div[1]/div/div/div/div/div/div//text()").extract())
+            #print  item['meta'] . join("foo")
             #item['creating'] = sel.xpath('a/@href').extract()
             #item['documentation'] = sel.xpath('text()').extract()
             #print item['overview']
